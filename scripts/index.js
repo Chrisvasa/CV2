85742e92fda0cc6981859b04754a48c74ba419b1
Chrisvasa <Christophervasankari@gmail.com>
2022-11-12 22:01:56 +0100

    Homepage - almost done

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,14 @@
 let url = "data.json";
+let headerDesc = document.querySelector('header').appendChild(document.createElement('p'))
+document.querySelector('main').appendChild(document.createElement('div')).className = "main-container";
+document.querySelector('main').appendChild(document.createElement('div')).className = "main-container-items";
+
+getData();
+hamburgerToggle();
+setTimeout(() => {
+    readMenuClick();
+}, 150); // Temporary fix -- Make it run after getData() is complete
+// Also show loading for slower speeds
 
 async function getData() {
     let response = await fetch(url);
@@ -6,23 +16,28 @@ async function getData() {
     if (response.ok) {
         let data = await response.json();
 
-        console.log(data.about.location)
-        printWork(data.work, "work")
+        printWork(data.work, "work", "default") // This will be the default shown on page load
         printWork(data.education, "education")
         printWork(data.skills, "skills")
+        headerDesc.textContent = data.about.description;
     }
     else {
         console.log("HTTP-Error:" + response.status);
     }
 }
 
-getData();
-hamburgerToggle();
+// Prints out the given section from the JSON file and gives it suiting class names and titles
+function printWork(dataType, title, selected) {
+    const divTitle = document.querySelector(`.main-container`).appendChild(document.createElement('div'))
+    divTitle.className = `main-item-${title}`;
 
-function printWork(dataType, title) {
-    const out = document.querySelector('main').appendChild(document.createElement('div'))
+    const titleOut = document.querySelector(`.main-item-${title}`).appendChild(document.createElement('h3'))
+    const out = document.querySelector('.main-container-items').appendChild(document.createElement('div'))
+
+    titleOut.className = title + "-title";
+    titleOut.textContent = title.toUpperCase();
     out.className = title;
-    document.querySelector(`.${out.className}`).appendChild(document.createElement('h3')).textContent = title.toUpperCase();
+
     dataType.forEach(value => {
         for (const key in value) {
             let output = document.querySelector(`.${out.className}`).appendChild(document.createElement('p'))
@@ -30,8 +45,16 @@ function printWork(dataType, title) {
             output.className = key;
         }
     });
+
+    if (selected === "default") {
+        divTitle.className += " selected";
+    }
+    else {
+        out.className = title + ` hide`;
+    }
 }
 
+// Toggle for the hamburger menu
 function hamburgerToggle() {
     const hamburgerClick = document.querySelector('.nav-toggle');
     hamburgerClick.addEventListener('click', function() {
@@ -39,3 +62,42 @@ function hamburgerToggle() {
         navContainer.classList.toggle('hidden');
     })
 }
+
+// Reads which of the menu items the user has clicked
+function readMenuClick() {
+    const mainMenuItems = document.querySelector('.main-container').children;
+
+    Array.from(mainMenuItems).forEach(function(mainMenuItem) {
+        mainMenuItem.addEventListener('click', function() {
+            hideMenuItem(mainMenuItems);
+            showMenuItem(mainMenuItem);
+        })
+    })
+}
+
+// Removes "selected" from classList and adds "hide" to classList
+// Will hide the menu items, so that only one item at a time is shown
+function hideMenuItem(mainMenuItems) {
+    const subMenuItems = document.querySelector('.main-container-items').children;
+
+    Array.from(mainMenuItems).forEach(function(mainMenuItem) {
+        mainMenuItem.classList.remove('selected')
+    });
+
+    Array.from(subMenuItems).forEach(function(item) {
+        item.classList.add('hide');
+    })
+}
+
+// Removes "hide" from classList and adds "selected" to classList
+// Which will show the menuItem that has been selected by the user
+function showMenuItem(menuItem) {
+    const split = menuItem.className.split('-')
+    const lastWord = split[split.length -1]
+    const testing = document.querySelector(`.${lastWord}`)
+
+    testing.classList.remove('hide');
+    menuItem.classList.add('selected')
+}
+
+
