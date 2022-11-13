640468f4916db33a15c128570388d3db5167e813
Chrisvasa <Christophervasankari@gmail.com>
2022-11-13 16:33:10 +0100

    Projects - w0rk

--- scripts/projects.js
+++ scripts/projects.js
@@ -1,8 +1,14 @@
-let url = "data.json"; 
-//'https://api.github.com/users/chrisvasa/repos?per_page=2&page=2'
+let url = "data.json";
+let headerDesc = document.querySelector('header').appendChild(document.createElement('p'))
+document.querySelector('main').appendChild(document.createElement('div')).className = "main-container";
+document.querySelector('main').appendChild(document.createElement('div')).className = "main-container-items";
 
 getData();
 hamburgerToggle();
+/* setTimeout(() => {
+    readMenuClick();
+}, 150); */ // Temporary fix -- Make it run after getData() is complete
+// Also show loading for slower speeds
 
 async function getData() {
     let response = await fetch(url);
@@ -10,68 +16,42 @@ async function getData() {
     if (response.ok) {
         let data = await response.json();
 
-        getImage(data.projects);
-        getCurrentSlide(data.projects);
-        printProject(data.projects, "projects")
+        printWork(data.projects, "Project1", 0, "default") // This will be the default shown on page load
+        printWork(data.projects, "Project2", 1)
+        readMenuClick();
     }
     else {
         console.log("HTTP-Error:" + response.status);
     }
 }
 
-function printProject(dataType, title) {
-    let i = 0;
+// Prints out the given section from the JSON file and gives it suiting class names and titles
+function printWork(dataType, title, number,  selected) {
+    const divTitle = document.querySelector(`.main-container`).appendChild(document.createElement('div'))
+    divTitle.className = `main-item-${title}`;
 
-    dataType.forEach(value => {
-        const out = document.querySelector('.portfolio-main-items').appendChild(document.createElement('div'))
-        out.className = title + i;
-        i++;
-        for (const key in value) {
-            let output = document.querySelector(`.${out.className}`).appendChild(document.createElement('p'))
+    const titleOut = document.querySelector(`.main-item-${title}`).appendChild(document.createElement('h3'))
+    const out = document.querySelector('.main-container-items').appendChild(document.createElement('div'))
+
+    titleOut.className = title + "-title";
+    titleOut.textContent = title.toUpperCase();
+    out.className = title;
+
+        for (const key in dataType[number]) {
             if (key == "image") {
                 break;
             }
-            output.textContent = value[key];
+            let output = document.querySelector(`.${out.className}`).appendChild(document.createElement('p'))
+            output.textContent = dataType[number][key];
             output.className = key;
+        };
+
+    if (selected === "default") {
+        divTitle.className += " selected";
     }
-})
-}
-
-//Image slider
-function getCurrentSlide(data) {
-    const sliderDots = document.querySelector('.slider-dots-container').children;
-    Array.from(sliderDots).forEach(function(item) {
-        item.addEventListener('click', function() {
-            console.log(item)
-            hideMenuItem(sliderDots)
-            item.classList.add('activated')
-            showMenuItem(item)
-        })
-    })
-}
-
-function hideMenuItem(mainMenuItems) {
-    const subMenuItems = document.querySelector('.portfolio-main-items').children;
-
-    Array.from(mainMenuItems).forEach(function(mainMenuItem) {
-        mainMenuItem.classList.remove('activated')
-    });
-
-    Array.from(subMenuItems).forEach(function(item) {
-        item.classList.add('hide');
-    })
-}
-
-function showMenuItem(menuItem) {
-    const subMenuItems = document.querySelector('.portfolio-main-items').children;
-    
-    const split = subMenuItems.className.split('-')
-    const lastWord = split[split.length -1]
-    const testing = document.querySelector(`.${lastWord}`)
-    console.log(testing)
-
-    menuItem.classList.remove('hide')
-    menuItem.classList.add('activated')
+    else {
+        out.className = title + ` hide`;
+    }
 }
 
 // Toggle for the hamburger menu
@@ -83,21 +63,42 @@ function hamburgerToggle() {
     })
 }
 
-// Gets the project image from JSON file
-function getImage(data) {
-    for (i = 0; i < data.length; i++) {
-        const portfolioImage = document.querySelector('.portfolio-container').appendChild(document.createElement('img'));
-        const slider = document.querySelector('.slider-dots-container').appendChild(document.createElement('span'));
+// Reads which of the menu items the user has clicked
+function readMenuClick() {
+    const mainMenuItems = document.querySelector('.main-container').children;
 
-        portfolioImage.src = data[i].image;
-        /* console.log(data[i].image) */
-        portfolioImage.className = i;
-        slider.className = i;
-
-        if (i === 0) {
-            slider.classList.add("activated");
-            portfolioImage.style.display = "block"
-        }
-    }
+    Array.from(mainMenuItems).forEach(function(mainMenuItem) {
+        mainMenuItem.addEventListener('click', function() {
+            hideMenuItem(mainMenuItems);
+            showMenuItem(mainMenuItem);
+        })
+    })
 }
+
+// Removes "selected" from classList and adds "hide" to classList
+// Will hide the menu items, so that only one item at a time is shown
+function hideMenuItem(mainMenuItems) {
+    const subMenuItems = document.querySelector('.main-container-items').children;
+
+    Array.from(mainMenuItems).forEach(function(mainMenuItem) {
+        mainMenuItem.classList.remove('selected')
+    });
+
+    Array.from(subMenuItems).forEach(function(item) {
+        item.classList.add('hide');
+    })
+}
+
+// Removes "hide" from classList and adds "selected" to classList
+// Which will show the menuItem that has been selected by the user
+function showMenuItem(menuItem) {
+    const split = menuItem.className.split('-')
+    const lastWord = split[split.length -1]
+    const testing = document.querySelector(`.${lastWord}`)
+    console.log(menuItem)
+
+    testing.classList.remove('hide');
+    menuItem.classList.add('selected')
+}
+
 
